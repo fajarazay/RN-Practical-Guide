4ccbff1a3fcf722741e93c0c566f138b4a893486
Fajar Septian <fajarajay10@gmail.com>
2019-07-08 10:16:16 +0700

    Adding a Button and Managing State

--- App.js
+++ App.js
@@ -6,14 +6,16 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, { Fragment, Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
+  Button
 } from 'react-native';
 
 import {
@@ -24,74 +26,98 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+console.disableYellowBox = true;
+
+//const App = () => {
+export default class App extends Component<Props> {
+
+  state = {
+    placeName: "",
+    places: []
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  }
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
+
+  render() {
+    const placesOutput = this.state.places.map((
+      place, index) => <Text key={index}>{place}</Text>
+    );
+
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            <View style={styles.container}>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  underlineColorAndroid='red'
+                  style={styles.placeInput}
+                  placeholder="type here"
+                  value={this.state.placeName}
+                  onChangeText={this.placeNameChangedHandler} >
+                </TextInput>
+                <Button
+                  color='red'
+                  style={styles.placeButton}
+                  title="Add"
+                  onPress={this.placeSubmitHandler}></Button>
+              </View>
+              <View>
+                {placesOutput}
+              </View>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+
+          </ScrollView>
+        </SafeAreaView>
+      </Fragment >
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: Colors.lighter,
+    //backgroundColor: Colors.lighter,
   },
-  body: {
-    backgroundColor: Colors.white,
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  inputContainer: {
+    //flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  placeInput: {
+    width: '70%'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  placeButton: {
+    width: '30%'
+  }
 });
 
-export default App;
+//export default App;
