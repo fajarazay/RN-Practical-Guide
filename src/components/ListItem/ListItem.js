a229e3a46f73d2254576b90e3a792abd32e620bf
Fajar Septian <fajarajay10@gmail.com>
2019-07-08 14:07:30 +0700

    reacting press event

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const listItem = (props) => (
-    <TouchableOpacity onPress={props.onItemPressed}>
+    <TouchableOpacity onPress={props.onItemDeleted}>
         <View style={styles.listItem}>
             <Text>{props.placeName}</Text>
         </View>
